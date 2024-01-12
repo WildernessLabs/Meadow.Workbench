36c26286c9e1890b43a6a5bf2348ab2077ff713c
Chris Tacke <ctacke@gmail.com>
2024-01-12 13:12:30 -0600

    starting menu work

--- Source/Meadow.Workbench/Meadow.Workbench/ViewModels/MainViewModel.cs
+++ Source/Meadow.Workbench/Meadow.Workbench/ViewModels/MainViewModel.cs
@@ -4,17 +4,22 @@ using ReactiveUI;
 using Splat;
 using System.Linq;
 using System.Reactive;
+using System.Threading.Tasks;
 
 namespace Meadow.Workbench.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     private UserControl _activeContent;
+    private IFeature? _activeFeature;
 
     internal FeatureService FeatureService { get; }
     private SettingsService SettingsService { get; }
 
     public ReactiveCommand<IFeature, Unit> FeatureSelectedCommand { get; }
+    public IReactiveCommand SettingsCommand { get; }
+    public IReactiveCommand UserCommand { get; }
+
     public UserControl Content { get => _activeContent; set => this.RaiseAndSetIfChanged(ref _activeContent, value); }
 
     public MainViewModel()
@@ -23,8 +28,10 @@ public class MainViewModel : ViewModelBase
         FeatureSelectedCommand = ReactiveCommand.Create<IFeature>(ActivateFeature);
         SettingsService = Locator.Current.GetService<SettingsService>();
 
+        SettingsCommand = ReactiveCommand.CreateFromTask(ShowSettings);
+        UserCommand = ReactiveCommand.CreateFromTask(ShowUserLogin);
+
         // select the first feature
-        // TODO: remember the last feature selected and restore it on app run
         SettingsService = Locator.Current.GetService<SettingsService>();
         var lastFeature = SettingsService.LastFeature;
 
@@ -33,8 +40,23 @@ public class MainViewModel : ViewModelBase
         Content = c;
     }
 
+    private async Task ShowUserLogin()
+    {
+    }
+
+    private async Task ShowSettings()
+    {
+    }
+
     private void ActivateFeature(IFeature feature)
     {
+        if (_activeFeature != null)
+        {
+            _activeFeature.IsActive = false;
+        }
+
+        _activeFeature = feature;
+        _activeFeature.IsActive = true;
         Content = feature.ViewInstance.Value;
         SettingsService.LastFeature = feature.Title;
     }
